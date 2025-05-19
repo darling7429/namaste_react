a1c0e43638febedcd2e6b66b2e008a8c79f6538b
darling7429 <53552021+darling7429@users.noreply.github.com>
2025-05-19 15:38:50 -0500

    added jsx code in to react project

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+// const h1 = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "i am from h1 with 🚀🚀🚀"),
+//     React.createElement("h1", {}, "iam from heading 2 with h1 tag ")
+//   ])
+// ], React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "i am from h1"),
+//     React.createElement("h1", {}, "iam from heading 2 with h1 tag ")
+//   ])); // creates a plain javascript element as react element
+
+
+const heading =<h1>HI from namaste react 🚀🚀🚀</h1>
+const root = ReactDOM.createRoot(document.getElementById("root")); //creates root element to make changes using react
+
+root.render(heading); // it converts the created react elements in to html elements and puts in container .
