7f8809448263d743d1b3704b9289a49021e4ccd3
darling7429 <53552021+darling7429@users.noreply.github.com>
2025-05-19 16:28:09 -0500

    functional components in to code

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// const h1 = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child1" }, [
-//     React.createElement("h1", {}, "i am from h1 with 🚀🚀🚀"),
-//     React.createElement("h1", {}, "iam from heading 2 with h1 tag ")
-//   ])
-// ], React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", {}, "i am from h1"),
-//     React.createElement("h1", {}, "iam from heading 2 with h1 tag ")
-//   ])); // creates a plain javascript element as react element
 
 
-const heading =<h1>HI from namaste react 🚀🚀🚀</h1>
+
+
+const Title=()=>
+(
+    <h1>hi iam from title component</h1>
+)
+
+
+const  Heading=()=>(
+    <>
+    <Title/>
+<h1>hi am from functional component</h1>
+</>
+    
+
+
+)
 const root = ReactDOM.createRoot(document.getElementById("root")); //creates root element to make changes using react
 
-root.render(heading); // it converts the created react elements in to html elements and puts in container .
+root.render(<Heading/>); // it converts the created react elements in to html elements and puts in container .
